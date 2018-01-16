1f994e79d646bb7e25ffe74d66f9b040cce91724
obiknows <sam.nnodim@gmail.com>
2018-01-16 00:27:07 -0800

    oh shit.. this pretty tight

--- tha-platform/obiknows-web/components/Header.vue
+++ tha-platform/obiknows-web/components/Header.vue
@@ -1,13 +1,18 @@
 <template>
-  <header>
-    <nav class="pa3 pa4-ns">
-      <a class="link dim black b f1 f-headline-ns tc db mb3 mb4-ns" href="#" title="Home">Site Name</a>
-      <div class="tc pb3">
-        <a class="link dim gray f6 f5-ns dib mr3" href="#" title="Home">Home</a>
-        <a class="link dim gray f6 f5-ns dib mr3" href="#" title="About">About</a>
-        <a class="link dim gray f6 f5-ns dib mr3" href="#" title="Store">Store</a>
-        <a class="link dim gray f6 f5-ns dib" href="#" title="Contact">Contact</a>
-      </div>
-    </nav>
+  <header class="bg-white black-80 tc pv4 avenir">
+  <!-- Logo -->
+  <img src="../assets/svg/pyramids.svg" height="64" width="64"/>
+  <!-- Title + Sub -->
+  <h1 class="mt0 mb0 baskerville fw1 f1">Obi Knows</h1>
+  <h2 class="mt2 mb0 f6 fw4 ttu tracked">website and sacred knowledge base</h2>
+  <!-- Nav -->
+  <nav class="bt bb tc mw7 center mt4">
+    <a class="f6 f5-l link bg-animate black-80 hover-bg-light-green dib pa3 ph4-l" href="/art">Art</a>
+    <a class="f6 f5-l link bg-animate black-80 hover-bg-light-blue dib pa3 ph4-l" href="/code">Code</a>
+    <a class="f6 f5-l link bg-animate black-80 hover-bg-light-pink dib pa3 ph4-l" href="/food">Food</a>
+    <a class="f6 f5-l link bg-animate black-80 hover-bg-light-yellow dib pa3 ph4-l" href="/researcg">Research</a>
+    <a class="f6 f5-l link bg-animate black-80 hover-bg-light-yellow dib pa3 ph4-l" href="/design">Design</a>
+    <a class="f6 f5-l link bg-animate black-80 hover-bg-light-yellow dib pa3 ph4-l" href="/contact">Contact</a>
+  </nav>
   </header>
 </template>
--- tha-platform/obiknows-web/pages/food.vue
+++ tha-platform/obiknows-web/pages/food.vue
@@ -1,7 +1,8 @@
 <template>
   <section class="container">
+    <img src="~assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
     <h1 class="title">
-      HOME
+      FOOD
     </h1>
     <ul class="users">
       <li v-for="(user, index) in users" :key="index" class="user">
@@ -17,13 +18,14 @@
 import axios from '~/plugins/axios'
 
 export default {
+  name: 'The Food Page',
   async asyncData () {
     let { data } = await axios.get('/api/users')
     return { users: data }
   },
   head () {
     return {
-      title: 'Users'
+      title: 'Food'
     }
   }
 }
