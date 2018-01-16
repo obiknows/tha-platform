85b869c6a25c5839b9074c3714f4333e1e8285a1
obiknows <sam.nnodim@gmail.com>
2018-01-15 22:51:12 -0800

    started up an express & nuxt app

--- tha-platform/pages/design.js
+++
@@ -1,9 +0,0 @@
-// design page (/design)
-import Layout from '../components/layouts/layout'
-
-export default () => 
-<div>
-  <Layout>
-    <h1>Welcome to obiknows design!</h1>
-  </Layout>
-</div>
